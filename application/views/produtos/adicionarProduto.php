7da54878f290dc83e2c95bd233fbfbf6de9a1487
Lucas Rocha <wltopos@gmail.com>
2022-11-07 23:46:43 -0300

    teste

--- application/views/produtos/adicionarProduto.php
+++ application/views/produtos/adicionarProduto.php
@@ -458,12 +458,7 @@
         }
     })
 
-    let file = 'https://cdn-cosmos.bluesoft.com.br/products/7898643250929';
-    function updateThumbnail(file) {
 
-        $('.drop-zone').append(`<div class="drop-zone__thumb" data-label="${file}" style="background-image: url(${file})"`);
-    
-    }
 
 
     // ===========================================================
