e157197136fc046960be240b552be270643716f9
Lucas Rocha <wltopos@gmail.com>
2022-11-08 00:37:10 -0300

    teste

--- application/views/produtos/adicionarProduto.php
+++ application/views/produtos/adicionarProduto.php
@@ -461,7 +461,7 @@
 
 
     function updateThumbnail(file) {
-        if($(".drop-zone__thumb") && is_string(file)){
+        if($(".drop-zone__thumb") && typeof file == "string"){
             $(".drop-zone__thumb").remove();
             $(".drop-zone__prompt").remove();
             $('.drop-zone').append(`<div class="drop-zone__thumb" data-label="${file}" style="background-size: contain; background-repeat: no-repeat; background-image: url(${file}); background-color: white;"></div>`);
