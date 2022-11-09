9926af844b564409d71e588f68e2bd62186a1324
Lucas Rocha <wltopos@gmail.com>
2022-11-08 21:25:34 -0300

    Corrige css

--- application/views/produtos/visualizarProduto.php
+++ application/views/produtos/visualizarProduto.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/visualizar.css" />
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/controllers/visualizarProduto.css" />
 
 <div class="widget-box">
 
