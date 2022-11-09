9d627c26225821b039b9f3fab60cf1bb7c630bbc
Lucas Rocha <wltopos@gmail.com>
2022-11-09 01:29:17 -0300

    teste

--- application/controllers/Produtos.php
+++ application/controllers/Produtos.php
@@ -442,7 +442,7 @@ class Produtos extends MY_Controller
         }
 
         $this->load->library('upload', $config);
-var_dump($this->upload->do_upload());
+var_dump($this->upload->do_upload('userfile'));
 
         if (!$this->upload->do_upload()) {
              $error = ['error' => $this->upload->display_errors()];
