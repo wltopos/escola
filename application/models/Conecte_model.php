e30dcd541404b198dc7445efbd7a1beafb6c92a3
Lucas Rocha <wltopos@gmail.com>
2022-11-20 18:47:58 -0300

    teste

--- application/models/Conecte_model.php
+++ application/models/Conecte_model.php
@@ -31,7 +31,7 @@ class Conecte_model extends CI_Model
 
     public function getLastOs($cliente)
     {
-        $this->db_empresa->from('comercial_os_clientes');
+        $this->db_empresa->from('comercial_os_clientes.*');
         $this->db_empresa->join('administrativo_funcionarios', 'comercial_os_clientes.administrativo_funcionario_id = administrativo_funcionario.id_administrativo_funcionario', 'left');
         $this->db_empresa->where('comercial_cliente_id', $cliente);
         $this->db_empresa->limit(5);
