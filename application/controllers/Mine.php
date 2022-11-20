9d0043994f22375bcd55b234bf95ca98b739cc1a
Lucas Rocha <wltopos@gmail.com>
2022-11-20 20:10:37 -0300

    teste

--- application/controllers/Mine.php
+++ application/controllers/Mine.php
@@ -8,7 +8,8 @@ class Mine extends CI_Controller
     {
         parent::__construct();
         $this->load->model('Conecte_model');
-       
+        var_dump($_SESSION);
+        
         
     }
 
