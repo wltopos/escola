bc550d2d342d48b306404cbb86a6750b4de0afa6
Lucas Rocha <wltopos@gmail.com>
2022-11-20 21:12:52 -0300

    teste

--- application/controllers/Mine.php
+++ application/controllers/Mine.php
@@ -232,7 +232,7 @@ class Mine extends CI_Controller
                 // Verificar credenciais do usuário
                 if (password_verify($password, $cliente->senha)) {
                     $session_data = ['nome' => $cliente->nomeCliente, 'cliente_id' => $cliente->id_comercial_cliente, 'email' => $cliente->email, 'conectado' => true, 'isCliente' => true];
-                    // $this->session->set_userdata($session_data);
+                    $this->session->set_userdata($session_data);
                     // log_info($_SERVER['HTTP_CLIENT_IP'] . 'Efetuou login no sistema');
                     echo json_encode(['result' => true]);
                 } else {
