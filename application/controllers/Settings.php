b949037b4115e3e7496d566808b6044b21a3e404
Lucas Rocha <wltopos@gmail.com>
2023-02-04 15:26:25 -0300

    debug

--- application/controllers/Settings.php
+++ application/controllers/Settings.php
@@ -178,6 +178,9 @@ class Settings extends MY_Controller
         $this->getLinkReturnData($id); //RETORNA DADOS DE TITULO E BANCO DE DADOS A PARTIR DO ID DO MODAL
 
         $this->data['result'] = $this->setdb_model->getTabelaQID("estoque_".$this->data['id']."s", '*', "id_estoque_".$this->data['id']."=" . $this->uri->segment(4));
+        
+        exit("estoque_".$this->data['id']."s". '*'. "id_estoque_".$this->data['id']."=" . $this->uri->segment(4));
+
         $a = "$id";
         $b = "descricao" . ucfirst($id);
         $c = 'urlLogo' . ucfirst($id);
