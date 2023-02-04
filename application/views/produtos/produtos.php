db0b8a79deb2ce8e9690c85601b1b5bed17fbc0d
Lucas Rocha <wltopos@gmail.com>
2023-02-04 09:14:22 -0300

    debug

--- application/views/produtos/produtos.php
+++ application/views/produtos/produtos.php
@@ -223,6 +223,10 @@
                     target: 0,
                     visible: false,
                 },
+                {
+                    target: 7,
+                    visible: false,
+                },
             ],
             "language": {
                // processing: '<i class="fa fa-spinner fa-spin fa-3x fa-aw"></i>',
