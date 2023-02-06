85aa704b43d718d2550b5aacfcf4b3a98062e223
Lucas Rocha <wltopos@gmail.com>
2023-02-06 17:07:01 -0300

    corrige id

--- application/controllers/Settings.php
+++ application/controllers/Settings.php
@@ -53,10 +53,10 @@ class Settings extends MY_Controller
             }
 
             if ($this->permission->checkPermission($this->session->userdata('permissao'), 'vProduto')) {
-                $v = '<a style="margin-right: 1%" href="' . site_url() . "settings/visualizar/".$this->data['id'] ."/" . $settings->{"id_estoque_".$this->data['id'] } . '" class="btn-nwe" title="Visualizar Setting"><i class="bx bx-show bx-xs"></i></a>  ';
+                $v = '<a style="margin-right: 1%" href="' . site_url() . "settings/visualizar/".$this->data['titlo'] ."/" . $settings->{"id_estoque_".$this->data['id'] } . '" class="btn-nwe" title="Visualizar Setting"><i class="bx bx-show bx-xs"></i></a>  ';
             }
             if ($this->permission->checkPermission($this->session->userdata('permissao'), 'eProduto')) {
-                $e = '<a style="margin-right: 1%" href="' . site_url() . "settings/editar/".$this->data['id'] ."/" . $settings->{"id_estoque_".$this->data['id']} . '" class="btn-nwe3" title="Editar Setting"><i class="bx bx-edit bx-xs"></i></a>';
+                $e = '<a style="margin-right: 1%" href="' . site_url() . "settings/editar/".$this->data['titulo'] ."/" . $settings->{"id_estoque_".$this->data['id']} . '" class="btn-nwe3" title="Editar Setting"><i class="bx bx-edit bx-xs"></i></a>';
             }
             if ($this->permission->checkPermission($this->session->userdata('permissao'), 'dProduto')) {
                 $d = '<a style="margin-right: 1%" href="#modal-excluir" role="button" data-toggle="modal" setting="' .$this->data['id']. '" idSetting="' . $settings->{"id_estoque_".$this->data['id']} . '" class="btn-nwe4" title="Excluir Setting"><i class="bx bx-trash-alt bx-xs"></i></a>';
