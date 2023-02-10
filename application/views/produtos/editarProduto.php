d5100446a0a48160134b31c08a989eb805476f43
Lucas Rocha <wltopos@gmail.com>
2023-02-10 18:21:15 -0300

    debug

--- application/views/produtos/editarProduto.php
+++ application/views/produtos/editarProduto.php
@@ -563,7 +563,7 @@
     let i = 0;
     $('#add-campo').click(function() {
 
-        {
+        
             i++;
             let campo = $('#addCampo option:selected').text();
             let idCampo = $('#addCampo option:selected').val();
@@ -592,17 +592,14 @@
                                         <div id="rm_${idCampo[0]}_${i}" class='control-group'>
                                             <label for='${idCampo[0]}' class='control-label'>${campo}<span class='required'>*</span></label>
                                             <div class='controls'>
-                                            <textarea onkeydown='handleEnter(event)' type='${idCampo[1]}'  id='${idCampo[0]}' name='addCampoInput[${idCampo[0]}_${i}]' rows="4" cols="50"></textarea>
-                                            <button title="remove campo" class="btn btn-danger" type="button"  onclick="removeCampo('#rm_${idCampo[0]}_${i}')" style="margin-left: 5px;">
+                                            <input onkeydown='handleEnter(event)' type='${idCampo[1]}'  id='${idCampo[0]}' name='addCampoInput[${idCampo[0]}_${i}]' value='' />
+                                            <button title="remove campo" class="btn btn-danger" type="button"  onclick="removeCampo('#rm_${idCampo[0]}_${i}')" style="margin-left: 5px; height: 33px;">
                                                 <i class="fa fa-minus"></i>
                                             </button>
                                             </div>
                                         </div>
                                         `);
-
-
             }
-        }
     });
 
     function btAddCampo() {
