d496546cac5a8b15686dd5201456b586aac9f00d
Lucas Rocha <wltopos@gmail.com>
2023-02-10 20:13:33 -0300

    debug

--- application/views/produtos/editarProduto.php
+++ application/views/produtos/editarProduto.php
@@ -538,7 +538,17 @@
             }
             if (idCampo[0] != "0" && i < 5 && idCampo[1] == 'textarea') {
              
-                $('#divAddCampo').append(`<textarea  ></textarea>`);
+                $('#divAddCampo').append(`
+                                        <div id='rm_${idCampo[0]}_${i}' class='control-group'>
+                                            <label for='${idCampo[0]}' class='control-label'>${campo}<span class='required'>*</span></label>
+                                            <div class='controls'>
+                                            <textarea  onkeydown='handleEnter(event)' id='${idCampo[0]}' name='addCampoInput[${idCampo[0]}_${i}]'  rows='5' cols='33' ></textarea>
+                                            <button title='remove campo' class='btn btn-danger' type='button'  onclick='removeCampo("#rm_${idCampo[0]}_${i}")' style='margin-left: 5px; height: 33px;'>
+                                                <i class='fa fa-minus'></i>
+                                            </button>
+                                            </div>
+                                        </div>
+                                        `);
             }
     });
 
@@ -555,25 +565,5 @@
     }
 
  
-    $("#add-field-button").click(function() {
-        var selectedField = $("#addCampo").val();
-        if (selectedField == "") {
-            alert("Selecione um campo adicional");
-            return;
-        }
 
-        var label = $("#field-label-input").val();
-        if (label == "") {
-            alert("Insira o rótulo do campo");
-            return;
-        }
-
-        var html = "<div>";
-        html += "<label>" + label + ":</label>";
-        html += "<input type='" + selectedField + "'>";
-        html += "</div>";
-        $("#additional-fields-container").append(html);
-
-        $("#field-label-input").val("");
-    });
 </script>
