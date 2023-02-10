04d75edc10bca8559731108b62fc3e39c97d4c65
Lucas Rocha <wltopos@gmail.com>
2023-02-10 20:57:21 -0300

    debug

--- application/views/produtos/editarProduto.php
+++ application/views/produtos/editarProduto.php
@@ -540,17 +540,7 @@
 
                 let textarea = $(`<textarea  onkeydown='handleEnter(event)' id='${idCampo[0]}' name='addCampoInput[${idCampo[0]}_${i}]'  rows='5' cols='33' >oi</textarea>`);
              
-                $('#divAddCampo').append(`
-                                            <div id='rm_${idCampo[0]}_${i}' class='control-group'>
-                                            <label for='${idCampo[0]}' class='control-label'>${campo}<span class='required'>*</span></label>
-                                            <div class='controls'>
-                                            ${textarea}
-                                            <button title='remove campo' class='btn btn-danger' type='button'  onclick='removeCampo("#rm_${idCampo[0]}_${i}")' style='margin-left: 5px; height: 33px;'>
-                                            <i class='fa fa-minus'></i>
-                                            </button>
-                                            </div>
-                                            </div>
-                                        `); 
+                $('#divAddCampo').append(`<div id='rm_${idCampo[0]}_${i}' class='control-group'><label for='${idCampo[0]}' class='control-label'>${campo}<span class='required'>*</span></label><div class='controls'>${textarea}<button title='remove campo' class='btn btn-danger' type='button' onclick='removeCampo("#rm_${idCampo[0]}_${i}")' style='margin-left: 5px; height: 33px;'><i class='fa fa-minus'></i></button></div></div>`); 
             }
     });
 
