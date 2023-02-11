c687ca928fa2345a220cd635eabfab91f880f955
Lucas Rocha <wltopos@gmail.com>
2023-02-10 23:11:43 -0300

    add tipo de campo adicional

--- application/views/produtos/adicionarProduto.php
+++ application/views/produtos/adicionarProduto.php
@@ -498,16 +498,31 @@
     let i = 0;
     $('#add-campo').click(function() {
 
-        {
-            i++;
-            let campo = $('#addCampo option:selected').text();
-            let idCampo = $('#addCampo option:selected').val();
+        i++;
+        let campo = $('#addCampo option:selected').text();
+        let idCampo = $('#addCampo option:selected').val();
+        idCampo = idCampo.split(',');
+        console.log(i);
+        if (idCampo[0] != "0" && i < 5 && idCampo[1] != 'textarea') {
 
-            if (idCampo != "0" && i < 5) {
+            $('#divAddCampo').append(`<div id="rm_${idCampo[0]}_${i}" class='control-group'>
+                                      <label for='${idCampo[0]}' class='control-label'>${campo}<span class='required'>*</span></label>
+                                      <div class='controls'>
+                                      <input onkeydown='handleEnter(event)' type='${idCampo[1]}'  id='${idCampo[0]}' name='addCampoInput[${idCampo[0]}_${i}]' value='' ${idCampo[1]=='color'?'style=" height: 33px;"':''}/>
+                                      <button title="remove campo" class="btn btn-danger" type="button"  onclick="removeCampo('#rm_${idCampo[0]}_${i}')" style="margin-left: 5px;">
+                                      <i class="fa fa-minus"></i></button></div></div>`);
 
-                $('#divAddCampo').append(`<div id="rm_${idCampo}_${i}" class='control-group'><label for='${idCampo}' class='control-label'>${campo}<span class='required'>*</span></label><div class='controls'><input required  onkeydown='handleEnter(event)' type='text'  id='${idCampo}' name='addCampoInput[${idCampo}_${i}]' value='' />   <button title="remove campo" class="btn btn-danger" type="button"  onclick="removeCampo('#rm_${idCampo}_${i}')" style="margin-left: 5px;"><i class="fa fa-minus"></i></button> </div> </div>`);
 
-            }
+        }
+        if (idCampo[0] != "0" && i < 2 && idCampo[1] == 'textarea') {
+            
+            let textarea = `<${idCampo[1]}  onkeydown='handleEnter(event)' id='${idCampo[0]}' name='addCampoInput[${idCampo[0]}_${i}]'  rows='5' cols='33' ></${idCampo[1]}>`;
+           
+            $('#divAddCampo').append(`<div id='rm_${idCampo[0]}_${i}' class='control-group'>
+                                        <label for='${idCampo[0]}' class='control-label'>${campo}<span class='required'>*</span></label>
+                                        <div class='controls'>${textarea}
+                                        <button title='remove campo' class='btn btn-danger' type='button' onclick='removeCampo("#rm_${idCampo[0]}_${i}")' style='margin-left: 5px; height: 33px;'>
+                                        <i class='fa fa-minus'></i></button></div></div>`);
         }
     });
 
