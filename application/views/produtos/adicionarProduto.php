d8e02184bcfe83f8129d5e791c057c8229cbd0c0
Lucas Rocha <wltopos@gmail.com>
2023-02-11 19:54:46 -0300

    add campoAdd

--- application/views/produtos/adicionarProduto.php
+++ application/views/produtos/adicionarProduto.php
@@ -307,14 +307,14 @@
                                 dadosCampo = dadosCampo.split('::');
                                 camposDB.forEach((campo) => {
                                     if (campo.id_estoque_addCampo == dadosCampo[0] && campo.tipoAddCampo != "textarea") {
-                                        $('#divAddCampo').append(`<div id='rm_${campo.siglaAddCampo}_${i}' class='control-group'>
+                                        $('#divAddCampo').append(`<div id='rm_${campo.siglaAddCampo}_${i}' class='control-group campoAdd'>
                                                             <label for='${campo.siglaAddCampo}_${i}' class='control-label'>${campo.addCampo}
                                                             <span class='required'>*</span></label>
                                                             <div class='controls'><input required  onkeydown='handleEnter(event)' type='${campo.tipoAddCampo}'  id='${campo.siglaAddCampo}_${i}' name='addCampoInput[${campo.siglaAddCampo}_${i}]' value='${dadosCampo[1]} ' ${campo.tipoAddCampo =='color'?'style=" height: 33px;  width: 16em;"':''} />
                                                             <button title="remove campo" class="btn btn-danger" type="button"  onclick="removeCampo('#rm_${campo.siglaAddCampo}_${i}')" style="margin-left: 5px;"><i class="fa fa-minus"></i></button> </div> </div>`);
                                     }
                                     if (campo.id_estoque_addCampo == dadosCampo[0] && campo.tipoAddCampo == "textarea") {
-                                        $('#divAddCampo').append(`<div id='rm_${campo.siglaAddCampo}_${i}' class='control-group'>
+                                        $('#divAddCampo').append(`<div id='rm_${campo.siglaAddCampo}_${i}' class='control-group campoAdd'>
                                                             <label for='${campo.siglaAddCampo}_${i}' class='control-label'><?= isset($r->addCampo) ? $r->addCampo : ''; ?>
                                                             <span class='required'>*</span></label><div class='controls'>
                                                             <${campo.tipoAddCampo} required  onkeydown='handleEnter(event)'  id='${campo.siglaAddCampo}_${i}' name='addCampoInput[${campo.siglaAddCampo}_${i}]' rows='5' cols='33' >  ${dadosCampo[1]} </${campo.tipoAddCampo}>
@@ -456,7 +456,7 @@
         idCampo = idCampo.split(',');
         if (idCampo[0] != "0" && i < 5 && idCampo[1] != 'textarea') {
 
-            $('#divAddCampo').append(`<div id="rm_${idCampo[0]}_${i}" class='control-group'>
+            $('#divAddCampo').append(`<div id="rm_${idCampo[0]}_${i}" class='control-group campoAdd'>
                                       <label for='${idCampo[0]}' class='control-label'>${campo}<span class='required'>*</span></label>
                                       <div class='controls'>
                                       <input onkeydown='handleEnter(event)' type='${idCampo[1]}'  id='${idCampo[0]}' name='addCampoInput[${idCampo[0]}_${i}]' value='' ${(idCampo[1]=='color')?'style=" height: 33px;  width: 16em;':''} />
@@ -469,7 +469,7 @@
 
             let textarea = `<${idCampo[1]}  onkeydown='handleEnter(event)' id='${idCampo[0]}' name='addCampoInput[${idCampo[0]}_${i}]'  rows='5' cols='33' ></${idCampo[1]}>`;
 
-            $('#divAddCampo').append(`<div id='rm_${idCampo[0]}_${i}' class='control-group'>
+            $('#divAddCampo').append(`<div id='rm_${idCampo[0]}_${i}' class='control-group campoAdd'>
                                         <label for='${idCampo[0]}' class='control-label'>${campo}<span class='required'>*</span></label>
                                         <div class='controls'>${textarea}
                                         <button title='remove campo' class='btn btn-danger' type='button' onclick='removeCampo("#rm_${idCampo[0]}_${i}")' style='margin-left: 5px; height: 33px;'>
