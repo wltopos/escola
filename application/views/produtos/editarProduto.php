976565350e4f46bbfe3f96a4996e9a56380182cf
Lucas Rocha <wltopos@gmail.com>
2023-02-10 21:31:18 -0300

    debug

--- application/views/produtos/editarProduto.php
+++ application/views/produtos/editarProduto.php
@@ -531,13 +531,9 @@
         }
         if (idCampo[0] != "0" && i < 5 && idCampo[1] == 'textarea') {
 
-            let textarea = $(`<textarea2>oi</textarea2>`);
+            let textarea = $(`<textarea>oi</textarea>`);
 
-            $('#divAddCampo').append(`<div id='rm_${idCampo[0]}_${i}' class='control-group'>
-                                        <label for='${idCampo[0]}' class='control-label'>${campo}<span class='required'>*</span></label>
-                                        <div class='controls'>
-                                        <button title='remove campo' class='btn btn-danger' type='button' onclick='removeCampo("#rm_${idCampo[0]}_${i}")' style='margin-left: 5px; height: 33px;'>
-                                        <i class='fa fa-minus'></i></button></div></div>`);
+            $('#divAddCampo').append(textarea);
         }
     });
 
