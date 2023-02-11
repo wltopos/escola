b8786463c46a99769ab1508f5268bb5f63d90728
Lucas Rocha <wltopos@gmail.com>
2023-02-10 21:35:34 -0300

    debug

--- application/views/produtos/editarProduto.php
+++ application/views/produtos/editarProduto.php
@@ -312,7 +312,7 @@
             const form = document.getElementById('formProduto');
             const index = [...form].indexOf(event.target);
             form.elements[index + 1].focus();
-            // event.preventDefault();
+            // event.preventDefault()
         }
     }
 
