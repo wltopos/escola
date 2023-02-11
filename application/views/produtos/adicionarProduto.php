e3b2d7c0188ea3ec718b8d76e05d9693605dd51d
Lucas Rocha <wltopos@gmail.com>
2023-02-10 23:17:47 -0300

    debug

--- application/views/produtos/adicionarProduto.php
+++ application/views/produtos/adicionarProduto.php
@@ -352,19 +352,10 @@
 
                                 camposDB.forEach((campo) => {
 
-                                    if (campo.id_estoque_addCampo == dadosCampo[0] && campo.tipoCampo != "textarea") {
-                                        $('#divAddCampo').append(`<div id='rm_${campo.siglaCampo}_${i}' class='control-group'>
-                                                            <label for='${campo.siglaCampo}_${i}' class='control-label'><?= isset($r->addCampo) ? $r->addCampo : ''; ?>
-                                                            <span class='required'>*</span></label>
-                                                            <div class='controls'><input required  onkeydown='handleEnter(event)' type='${campo.tipoCampo}'  id='${campo.siglaCampo}_${i}' name='addCampoInput[${campo.siglaCampo}_${i}]' value='${dadosCampo[1]} ' />
-                                                            <button title="remove campo" class="btn btn-danger" type="button"  onclick="removeCampo('#rm_${campo.siglaCampo}_${i}')" style="margin-left: 5px;"><i class="fa fa-minus"></i></button> </div> </div>`);
-                                    }
-                                    if (campo.id_estoque_addCampo == dadosCampo[0] && campo.tipoCampo == "textarea") {
-                                        $('#divAddCampo').append(`<div id='rm_${campo.siglaCampo}_${i}' class='control-group'>
-                                                            <label for='${campo.siglaCampo}_${i}' class='control-label'><?= isset($r->addCampo) ? $r->addCampo : ''; ?>
-                                                            <span class='required'>*</span></label><div class='controls'>
-                                                            <${campo.tipoCampo} required  onkeydown='handleEnter(event)'  id='${campo.siglaCampo}_${i}' name='addCampoInput[${campo.siglaCampo}_${i}]' rows='5' cols='33' >  ${dadosCampo[1]} </${campo.tipoCampo}>
-                                                            <button title="remove campo" class="btn btn-danger" type="button"  onclick="removeCampo('#rm_${campo.siglaCampo}_${i}')" style="margin-left: 5px;"><i class="fa fa-minus"></i></button> </div> </div>`);
+                                    if (campo.id_estoque_addCampo == dadosCampo[0]) {
+                                        console.log(dadosCampo);
+                                        console.log(campo.addCampo);
+                                        $('#divAddCampo').append(`<div id='rm_${campo.siglaAddCampo}_${i}' class='control-group addCampo'><label for='${campo.siglaAddCampo}_${i}' class='control-label'>${campo.addCampo}<span class='required'>*</span></label><div class='controls'><input required  onkeydown='handleEnter(event)' type='${campo.tipoAddCampo}'  id='${campo.siglaAddCampo}_${i}' name='addCampoInput[${campo.id_estoque_addCampo}_${i}]' value='${dadosCampo[1]} ' />   <button title="remove campo" class="btn btn-danger" type="button"  onclick="removeCampo('#rm_${campo.siglaAddCampo}_${i}')" style="margin-left: 5px;"><i class="fa fa-minus"></i></button> </div> </div>`);
                                     }
                                 });
 
@@ -500,7 +491,7 @@
 
     }
 
-
+  
     // ===========================================================
     // SCRIPT BOTÃO ADICIONAR CAMPO
     // ===========================================================
@@ -524,9 +515,9 @@
 
         }
         if (idCampo[0] != "0" && i < 2 && idCampo[1] == 'textarea') {
-
+            
             let textarea = `<${idCampo[1]}  onkeydown='handleEnter(event)' id='${idCampo[0]}' name='addCampoInput[${idCampo[0]}_${i}]'  rows='5' cols='33' ></${idCampo[1]}>`;
-
+           
             $('#divAddCampo').append(`<div id='rm_${idCampo[0]}_${i}' class='control-group'>
                                         <label for='${idCampo[0]}' class='control-label'>${campo}<span class='required'>*</span></label>
                                         <div class='controls'>${textarea}
