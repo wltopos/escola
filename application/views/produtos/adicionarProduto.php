8556cd433618950d666cc5245a8c84846fb5ceca
Lucas Rocha <wltopos@gmail.com>
2023-02-11 20:18:02 -0300

    debug

--- application/views/produtos/adicionarProduto.php
+++ application/views/produtos/adicionarProduto.php
@@ -216,7 +216,7 @@
         //Select com buscador
         $('select').select2();
         $('.wh30').select2(({
-            width: '8em'
+            width: '9em'
         }));
         //validação de campos
         $(".money").maskMoney();
