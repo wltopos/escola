10aeafdc461a71aac6c14522fe5ebea0d40b8312
Lucas Rocha <wltopos@gmail.com>
2023-02-13 20:05:38 -0300

    debug

--- application/controllers/Settings.php
+++ application/controllers/Settings.php
@@ -174,7 +174,7 @@ class Settings extends MY_Controller
         $this->load->model('produtos_model');
         
         $this->getLinkReturnData($id); //RETORNA DADOS DE TITULO E BANCO DE DADOS A PARTIR DO ID DO MODAL
-       
+       print_r("estoque_".mb_strtoupper($this->data['id'])."s". '*'. "id_estoque_".$this->data['id']."=" . $this->uri->segment(4));
         $this->data['result'] = $this->setdb_model->getTabelaQID("estoque_".mb_strtoupper($this->data['id'])."s", '*', "id_estoque_".$this->data['id']."=" . $this->uri->segment(4));
 
   
