cb6f75e1352f1dc5825706eb8a4de2458d6366a5
Lucas Rocha <wltopos@gmail.com>
2023-02-13 18:51:37 -0300

    debug

--- application/views/produtos/adicionarProduto.php
+++ application/views/produtos/adicionarProduto.php
@@ -458,12 +458,12 @@
         idCampo = idCampo.split(',');
         if (idCampo[0] != "0" && i < 5 && idCampo[1] != 'textarea') {
 
-            $('#divAddCampo').append(`<div id="rm_${idCampo[0]}_${i}" class='control-group campoAdd'>
+            $('#divAddCampo').append(`<div id='rm_${idCampo[0]}_${i}' class='control-group campoAdd'>
                                       <label for='${idCampo[0]}' class='control-label'>${campo}<span class='required'>*</span></label>
                                       <div class='controls'>
-                                      <input onkeydown='handleEnter(event)' type='${idCampo[1]}'  id='${idCampo[0]}' name='addCampoInput[${idCampo[0]}_${i}]' value='' ${(idCampo[1]=='color')?'style=" height: 33px;  width: 16em;':''} />
-                                      <button title="remove campo" class="btn btn-danger" type="button"  onclick="removeCampo('#rm_${idCampo[0]}_${i}')" style="margin-left: 5px;">
-                                      <i class="fa fa-minus"></i></button></div></div>`);
+                                      <input onkeydown='handleEnter(event)' type='${idCampo[1]}'  id='${idCampo[0]}' name='addCampoInput[${idCampo[0]}_${i}]' value='' ${(idCampo[1]=='color')?'style=" height: 33px;  width: 16em;"':''} />
+                                      <button title='remove campo' class='btn btn-danger' type='button'  onclick='removeCampo("#rm_${idCampo[0]}_${i}")' style='margin-left: 5px;'>
+                                      <i class='fa fa-minus'></i></button></div></div>`);
 
 
         }
@@ -474,7 +474,7 @@
             $('#divAddCampo').append(`<div id='rm_${idCampo[0]}_${i}' class='control-group campoAdd'>
                                         <label for='${idCampo[0]}' class='control-label'>${campo}<span class='required'>*</span></label>
                                         <div class='controls'>${textarea}
-                                        <button title='"remove campo"' class='"btn btn-danger"' type='button' onclick='removeCampo("#rm_${idCampo[0]}_${i}")' style='margin-left: 5px; height: 33px;'>
+                                        <button title='remove campo' class='btn btn-danger' type='button' onclick='removeCampo("#rm_${idCampo[0]}_${i}")' style='margin-left: 5px; height: 33px;'>
                                         <i class='fa fa-minus'></i></button></div></div>`);
         }
     });
