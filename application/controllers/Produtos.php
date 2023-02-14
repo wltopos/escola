bd9a178a854837b1b24a7dbf287a8f20539987fa
Lucas Rocha <wltopos@gmail.com>
2023-02-14 10:16:33 -0300

    debug

--- application/controllers/Produtos.php
+++ application/controllers/Produtos.php
@@ -477,7 +477,7 @@ class Produtos extends MY_Controller
               try{
                 
                 $file = $this->setdb_model->getTabelaQID("estoque_produtos", '*', "id_estoque_produto=" . $setting);
-                print_r(file_exists('sistema_dev/assets/uploads/db_wltopos/imagemProdutos/7a06afd5db315b2d979c6e42ec34e0e8.png'));
+                print_r(file_exists('/sistema_dev/assets/uploads/db_wltopos/imagemProdutos/7a06afd5db315b2d979c6e42ec34e0e8.png'));
                 exit('sistema_dev/assets/uploads/db_wltopos/imagemProdutos/7a06afd5db315b2d979c6e42ec34e0e8.png');
                 If(file_exists('sistema_dev/assets/uploads/db_wltopos/imagemProdutos/7a06afd5db315b2d979c6e42ec34e0e8.png')){
                     exit('Imagem localizada na pasta');
